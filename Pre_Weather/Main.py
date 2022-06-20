e55da8cb0219161bf6243704bdde23504d1dc357
Routhleck <1310722434@qq.com>
2022-06-20 20:53:20 +0800

    0620 update 增加风速

--- Pre_Weather/Main.py
+++ Pre_Weather/Main.py
@@ -29,32 +29,35 @@ all_low_t = []
 all_rainfall = []
 all_pressure = []
 all_wind = []
+all_windSpeed = []
 for a in range(1, 7):
     today = DT.datetime.now()
     time = (today + DT.timedelta(days=a)).date()
     print(time.year, '/', time.month, '/', time.day,
-          ': 平均气温',  preds[a][0], '℃ ',
-          '最高气温', preds[a][1], '℃ ',
-          '最低气温', preds[a][2], '℃ ',
-          "降雨量", preds[a][3], "mm ",
-          '气压', preds[a][4], 'hPa ', end=''
+          ': 平均气温',  round(preds[a][0], 2), '℃ ',
+          '最高气温', round(preds[a][1], 2), '℃ ',
+          '最低气温', round(preds[a][2], 2), '℃ ',
+          "降雨量", round(preds[a][3], 2), "mm ",
+          '气压', round(preds[a][4], 2), 'hPa ', end=''
           )
     if preds[a][5] <90:
-        print('东北风', preds[a][5], '° ')
+        print('东北风', round(preds[a][5], 2), '° ', end='')
     elif preds[a][5] <180:
-        print('东南风', preds[a][5], '° ')
+        print('东南风', round(preds[a][5], 2), '° ', end='')
     elif preds[a][5] <270:
-        print('西南风', preds[a][5], '° ')
+        print('西南风', round(preds[a][5], 2), '° ', end='')
     elif preds[a][5] <360:
-        print('西北风', preds[a][5], '° ')
+        print('西北风', round(preds[a][5], 2), '° ', end='')
+    print('风速', round(preds[a][6], 2), 'km/h',)
     all_ave_t.append(preds[a][0])
     all_high_t.append(preds[a][1])
     all_low_t.append(preds[a][2])
     all_rainfall.append(preds[a][3])
     all_pressure.append(preds[a][4])
     all_wind.append(preds[a][5])
+    all_windSpeed.append(preds[a][6])
 
-temp = {"ave_t": all_ave_t, "high_t": all_high_t, "low_t": all_low_t, "rainfall": all_rainfall, "pressure": all_pressure}
+temp = {"ave_t": all_ave_t, "high_t": all_high_t, "low_t": all_low_t, "rainfall": all_rainfall, "pressure": all_pressure, "windSpeed": all_windSpeed}
 # 绘画折线图
 plt.plot(range(1, 7), temp["ave_t"], color="green", label="ave_t")
 plt.plot(range(1, 7), temp["high_t"], color="red", label="high_t")
@@ -73,5 +76,11 @@ plt.show()
 plt.plot(range(1, 7), temp["pressure"], color="black", label="pressure")
 plt.legend()
 plt.ylabel("hPa")
+plt.xlabel("day")
+plt.show()
+#风速显示
+plt.plot(range(1, 7), temp["windSpeed"], color="black", label="windSpeed")
+plt.legend()
+plt.ylabel("km/h")
 plt.xlabel("day")
 plt.show()
---
+++ doc/upload/源码/Pre_Weather/Write.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/12/16
+# @Author: Eritque arcus
+# @File: Write.py
+from calendar import isleap
+import re
+from bs4 import BeautifulSoup
+from GetData import GetData
+import datetime as DT
+import csv
+
+
+def a(t):
+    return t.replace(" - ", "0")
+
+
+# 功能: 写csv
+def write(years, b, c):
+    """
+    :param years: [开始日期距离现在的年份, 结束日期距离现在的年份]
+    :param b: [开始日期距离现在日期的天数, 结束日期距离现在日期的天数]
+    :param c: csv文件名
+    :return: None
+    """
+    # 1. 创建文件对象
+    f = open(c, 'w', encoding='utf-8', newline='')
+
+    # 2. 基于文件对象构建 csv写入对象
+    csv_writer = csv.writer(f)
+
+    # 3. 构建列表头
+    # , "negAve", "negMax", "negMin"
+    csv_writer.writerow(["Time", "Ave_t", "Max_t", "Min_t", "Prec", "SLpress", "Winddir", "Windsp", "Cloud"])
+    # 取现在日期
+    today = DT.datetime.today()
+    # 闰年片段
+    st = isleap(today.year)
+    # 取20天前日期
+    week_ago = (today - DT.timedelta(days=b[0])).date()
+    # 20天后
+    week_pre = (today + DT.timedelta(days=b[1])).date()
+    if week_ago.month + week_pre.month == 3 or week_ago.month + week_pre.month == 5:
+        if week_ago.month == 2 and not st == isleap(today.year - years[0]):
+            if st:
+                # 今年是，去年或未来不是，所以-1
+                week_ago -= DT.timedelta(days=1)
+            else:
+                # 今年不是，去年或未来是，所以+1
+                week_ago += DT.timedelta(days=1)
+        if week_pre.month == 2 and not st == isleap(today.year - years[1]):
+            if st:
+                # 今年是，去年或未来不是，所以要-1
+                week_pre -= DT.timedelta(days=1)
+            else:
+                # 今年不是，去年或未来是，所以+1
+                week_pre += DT.timedelta(days=1)
+    # 城市id 广州59287 青岛 54857 北京54511
+    id = "54511"
+    # 爬取数据链接
+    url = "http://www.meteomanz.com/sy2?l=1&cou=2250&ind=" + id + "&d1=" + str(week_ago.day).zfill(2) + "&m1=" + str(
+        week_ago.month).zfill(2) + "&y1=" + str(week_ago.year - years[0]) + "&d2=" + str(week_pre.day).zfill(
+        2) + "&m2=" + str(week_pre.month).zfill(2) + "&y2=" + str(week_pre.year - years[1])
+    # 显示获取数据集的网址
+    print(url)
+    g = GetData(url).Get()
+    # beautifulsoup解析网页
+    soup = BeautifulSoup(g, "html5lib")
+    # 取<tbody>内容
+    tb = soup.find(name="tbody")
+    # 取tr内容
+    past_tr = tb.find_all(name="tr")
+    for tr in past_tr:
+        # 取tr内每个td的内容
+        text = tr.find_all(name="td")
+        flag = False
+        negA = negMax = negMin = False
+        for i in range(0, len(text)):
+            if i == 0:
+                text[i] = text[i].a.string
+                # 网站bug，会给每个月第0天，比如 00/11/2020,所以要drop掉
+                if "00/" in text[i]:
+                    flag = True
+            elif i == 8:
+                # 把/8去掉，网页显示的格式
+                text[i] = text[i].string.replace("/8", "")
+            elif i == 5:
+                # 去掉单位
+                text[i] = text[i].string.replace(" Hpa", "")
+            elif i == 6:
+                # 去掉风力里括号内的内容
+                text[i] = re.sub(u"[º(.*?|N|W|E|S)]", "", text[i].string)
+            else:
+                # 取每个元素的内容
+                text[i] = text[i].string
+            # 丢失数据都取2(简陋做法)
+            # 这么做 MAE=3.6021
+            text[i] = "2" if text[i] == "-" else text[i]
+            text[i] = "2" if text[i] == "Tr" else text[i]
+        text = text[0:9]
+        # ext += [str(int(negA)), str(int(negMax)), str(int(negMin))]
+        # 4. 写入csv文件内容
+        if not flag:
+            csv_writer.writerow(text)
+    # 5. 关闭文件
+    f.close()
