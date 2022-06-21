bef48ed66f9e321ad99425ed70fc21dde9ddc1b3
Shichao <92007182+Shigakki@users.noreply.github.com>
2022-06-21 15:09:04 +0800

    小的更新
    update

--- Pre_Weather/Main_ui.py
+++ Pre_Weather/Main_ui.py
@@ -14,7 +14,7 @@ def predict(city):
     preds = model.predict(r[1])
     print(len(preds))
     # 打印结果到控制台
-    print("未来6天预测")
+    print("未来7天预测")
     all_ave_t = []
     all_high_t = []
     all_low_t = []
